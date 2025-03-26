8c9eac81e5c276329f96e6b0d209e2ece33120f3
Muhammad Iqbal Gobel <121793805+muhammadiqbalg@users.noreply.github.com>
2025-03-26 21:59:54 +0700

    Initialized Project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-header></app-header>
+  <div class="container"></div>
+  <app-footer></app-footer>
+</template>
+
+<script>
+import compContact from './components/contact/index.vue';
+
+export default {
+  components: {
+    compContact
+  }
+}
+</script>
---
+++ src/components/header_footer/Header.vue
@@ -0,0 +1,15 @@
+<template>
+    <head>
+        Forms
+    </head>
+</template>
+
+<script></script>
+
+<style>
+body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Roboto', 'sans-serif';
+}
+</style>
